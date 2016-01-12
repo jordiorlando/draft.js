143b9300cc16189ab13ef48e62711f40420ada74
Jordi Orlando <jordi.orlando@gmail.com>
2016-01-11 21:55:19 -0600

    Initial support for relative transformations

--- src/mixins/position.js
+++ src/mixins/position.js
@@ -8,6 +8,14 @@ Draft.mixins.position = {
   },
 
   pos: function() {
-    return this.position(arguments);
+    return this.position.apply(this, arguments);
+  },
+
+  translate: function(x, y, z) {
+    x = this.prop('x') + x || 0;
+    y = this.prop('y') + y || 0;
+    z = this.prop('z') + z || 0;
+
+    return this.position(x, y, z);
   }
 };
--- src/mixins/rotation.js
+++ src/mixins/rotation.js
@@ -8,6 +8,14 @@ Draft.mixins.rotation = {
   },
 
   rot: function() {
-    return this.rotation(arguments);
+    return this.rotation.apply(this, arguments);
+  },
+
+  rotate: function(α, β, γ) {
+    α = this.prop('α') + α || 0;
+    β = this.prop('β') + β || 0;
+    γ = this.prop('γ') + γ || 0;
+
+    return this.position(α, β, γ);
   }
 };
