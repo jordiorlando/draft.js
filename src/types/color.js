fed884870e2f396c9833eca3e9482c674162a4cc
Jordi Orlando <jordi.orlando@gmail.com>
2016-02-01 20:51:48 -0600

    Make color/opacity test code more concise

--- src/types/color.js
+++ src/types/color.js
@@ -1,18 +1,10 @@
 draft.types.color = function color(val) {
-  if (val == undefined) {
-    return val;
-  }
-
   var hex = /^#?([a-f0-9]{6}|[a-f0-9]{3})$/i;
 
   var rgb255 = '([01]?\\d\\d?|2[0-4]\\d|25[0-5])';
   var rgb = new RegExp(`^rgb\\(${rgb255}\\, ?${rgb255}\\, ?${rgb255}\\)$`, 'i');
 
-  if (hex.test(val)) {
-    return val;
-  } else if (rgb.test(val)) {
+  if (val === undefined || val === null || hex.test(val) || rgb.test(val)) {
     return val;
   }
-
-  return undefined;
 };
--- src/types/opacity.js
+++ src/types/opacity.js
@@ -1,13 +1,9 @@
 draft.types.opacity = function opacity(val) {
-  if (val == undefined) {
-    return val;
-  }
-
   var from0to1 = /^(0(\.\d*)?|1(\.0*)?)$/;
 
-  if (from0to1.test(val)) {
+  if (val === undefined || val === null) {
+    return val;
+  } else if (from0to1.test(val)) {
     return parseFloat(val, 10);
   }
-
-  return undefined;
 };
